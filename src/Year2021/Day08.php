e90241c8cf237d007ed502bbd25fd4a4ccb9ce8e
aex <aex@localhost.localdomain>
2022-01-07 23:11:09 +0100

    refactored day8, added tests

--- src/Year2021/Day08.php
+++ src/Year2021/Day08.php
@@ -22,25 +22,16 @@ class Day08
     {
         echo "\n ### --- Day 8: Seven Segment Search Part1! --- ### \n";
 
-        $count = 0;
+        $count                  = 0;
 
         foreach ($this->data as $key => $row) {
-            [$input, $output] = $this->parseRow($row);
+            [$input, $output]   = $this->parseRow($row);
             foreach ($output as $value) {
                 
-                if (strlen($value) === 2) {
-                    $count++;
-                }
+                $value_len      = strlen($value);
+                $len_to_count   = [2,3,4,7];
 
-                if (strlen($value) === 3) {
-                    $count++;
-                }
-
-                if (strlen($value) === 4) {
-                    $count++;
-                }
-
-                if (strlen($value) === 7) {
+                if (in_array($value_len, $len_to_count)) {
                     $count++;
                 }
             }
@@ -53,21 +44,20 @@ class Day08
     {
         echo "\n ### --- Day 8: Seven Segment Search Part2! --- ### \n"; 
 
-        $sum = 0;
-        foreach ($this->data as $key => $row) {
-            [$input, $output] = $this->parseRow($row);
+        $sum                    = 0;
+        foreach ($this->data as $row) {
+            [$input, $output]   = $this->parseRow($row);
             
-            $map = $this->generateMap($input);
-            $number = '';
+            $map                = $this->generateMap($input);
+            $number             = '';
             foreach ($output as $value) {
-                $number .= $this->decode($value, $map);
+                $number        .= $this->decode($value, $map);
             }
 
-            $sum += $number;
+            $sum               += $number;
         }
 
         return $sum;
-
     }
 
     protected function parseRow($row)
@@ -80,13 +70,11 @@ class Day08
     protected function generateMap(array $input)
     {
         $map = [
-            0 => false,
-            1 => false,
-            2 => false,
-            3 => false,
-            4 => false,
-            5 => false,
-            6 => false,
+                0 => false,
+        1 => false,     2 => false,
+                3 => false,
+        4 => false,     5 => false,
+                6 => false,
         ];
 
         $segments_1 = $this->getInputByLen($input, 2);
@@ -97,90 +85,62 @@ class Day08
         $segments_4 = str_split($segments_4[0]);
         $segments_7 = str_split($segments_7[0]);
 
-
-        $map[0] = (array_diff($segments_7, $segments_1));
+        // get segment #0 by finding the char that is not in number 1 but in number 7
+        $map[0]     = implode('', (array_diff($segments_7, $segments_1)));
         
-
         $numbers_with_6_segments = $this->getInputByLen($input, 6);
         $numbers_with_5_segments = $this->getInputByLen($input, 5);
 
-        $almost_9 = array_unique(array_merge($segments_4, $segments_7));
-        asort($almost_9);
-        foreach ($numbers_with_6_segments as $key => $number) {
-            $number = str_split($number);
-            asort($number);
-            $diff = array_diff($number, $almost_9);
-            if (count($diff) === 1) {
-                $map[6] = $diff;
-                break;
-            }
-            
-        }
+        // building the number 9 with segment #6 misssing
+        $almost_9   = array_unique(array_merge($segments_4, $segments_7));
+        // get bottom segment going over all 6 segment numbers, find the one that is different by segment
+        // that one segment is the segment #6
+        $map[6]     = $this->findMissingChar($numbers_with_6_segments, $almost_9);
         
         $figure_8   = ['a', 'b', 'c', 'd', 'e', 'f', 'g'];
-        $almost_8   = array_unique(array_merge($segments_4, $segments_7, $map[6]));
-        $map[4]     = array_diff($figure_8, $almost_8);
+        // merge the segments for number 7 + 4 and #6
+        // the one mising will be the segment #4
+        $almost_8   = array_unique(array_merge($segments_4, $segments_7, [$map[6]]));
+        $map[4]     = implode('', array_diff($figure_8, $almost_8));
         
-        $almost_0   = array_unique(array_merge($segments_7, $map[4], $map[6]));
-        asort($almost_0);
-        foreach ($numbers_with_6_segments as $key => $number) {
-            $number = str_split($number);
-            asort($number);
-            $diff = array_diff($number, $almost_0);
-            if (count($diff) === 1) {
-                $map[1] = $diff;
-                break;
-            }   
-        }
+        // merge the segments for number 7 with #6 segment and #4 segment
+        // find the number with one differing segment, that will be number 0, the differing segment is #1
+        $almost_0   = array_merge($segments_7, [$map[4]], [$map[6]]);
+        $map[1]     = $this->findMissingChar($numbers_with_6_segments, $almost_0);
 
-        $figure_8   = ['a', 'b', 'c', 'd', 'e', 'f', 'g'];
-        $almost_8   = array_unique(array_merge($almost_0, $map[1]));
-        $map[3]     = array_diff($figure_8, $almost_8);
+        $almost_8   = array_merge($almost_0, [$map[1]]);
+        $map[3]     = implode('', array_diff($figure_8, $almost_8));
 
+        $almost_2   = [$map[0], $map[3], $map[4], $map[6]];
+        $map[2]     = $this->findMissingChar($numbers_with_5_segments, $almost_2);
 
-        $almost_2   = array_unique(array_merge($map[0], $map[3], $map[4], $map[6]));
-        asort($almost_2);
-        foreach ($numbers_with_5_segments as $key => $number) {
-            $number = str_split($number);
-            asort($number);
-            $diff = array_diff($number, $almost_2);
-            if (count($diff) === 1) {
-                $map[2] = $diff;
-                break;
-            }   
-        }
-
-        
-        $map[5]     = array_diff($segments_1, $map[2]);
-
-        foreach($map as $key => $value){
-            if (is_array($value)) {
-                foreach ($value as $val) {
-                    $map[$key] = $val;
-                }
-            }
-        }
-
-
+        $map[5]     = implode('', array_diff($segments_1, [$map[2]]));
         return $map;
-        /*
-        ab: 1
-        gcdfa: 2
-        fbcad: 3
-        eafb: 4
-        cdfbe: 5
-        cdfgeb: 6
-        dab: 7
-        acedgfb: 8
-        cefabd: 9
-        cagedb: 0
-        */
+    }
+
+    protected function findMissingChar($list, $to_exclude)
+    {
+        $result     = 0;
+        asort($to_exclude);
+        foreach ($list as $key => $number) {
+            $number = str_split($number);
+            asort($number);
+
+            $diff   = array_diff($number, $to_exclude);
+
+            if (count($diff) === 1) {
+                $result = implode('', $diff);
+                break;
+            }   
+        }
+
+        return $result;
     }
 
     protected function getInputByLen(array $data, int $len):array
     {
         $result             = [];
-        foreach ($data as $key => $value) {
+        foreach ($data as $value) {
             if (strlen($value) === $len) {
                 $result[]   = $value;
             }
@@ -188,6 +148,7 @@ class Day08
         return $result;
     }
 
+    // sort input convert to string, build string for each number from map, return comparison
     protected function decode(string $value, array $map):int
     {
         $mapping = [
@@ -203,16 +164,15 @@ class Day08
             '0' => [$map[0], $map[1], $map[2], $map[4], $map[5], $map[6]],
         ];
     
-        $value = str_split($value);
+        $value          = str_split($value);
         asort($value);
-        $a = implode('',$value);
+        $number_string  = implode('',$value);
 
         foreach ($mapping as $number => $segments) {
             asort($segments);
-            $b = implode('',$segments);
+            $map_string = implode('', $segments);
             
-
-            if ($a === $b) {
+            if ($number_string === $$map_string) {
                 return $number;
             }
         }
--- tests/Year2021/Day08Test.php
+++ tests/Year2021/Day08Test.php
@@ -7,7 +7,15 @@ use function PHPUnit\Framework\assertContains;
 
 class Day08Helper extends Day08
 {
-    
+    public function publicParseRow(string $row)
+    {
+        return $this->parseRow($row);
+    }
+
+    public function publicGenerateMap (array $input)
+    {
+        return $this->generateMap($input);
+    }
 }
 
 final class Day08Test extends TestCase
@@ -19,7 +27,37 @@ final class Day08Test extends TestCase
         $parser     = new Solver\InputParser($this->data_path);
         $solver     = new Day08Helper($parser->getData());
 
-        //$this->assertEquals('26', $solver->getResult(1));
+        $this->assertEquals('26', $solver->getResult(1));
         $this->assertEquals('61229', $solver->getResult(2));
     }
+
+    public function testParseRow()
+    {
+        $solver     = new Day08Helper([]);
+        [$input, $output]     = $solver->publicParseRow('gcafb gcf dcaebfg ecagb gf abcdeg gaef cafbge fdbac fegbdc | fgae cfgab fg bagce');
+
+        $this->assertIsArray($input);
+        $this->assertIsArray($output);
+        $this->assertEquals('gcafb', $input[0]);
+        $this->assertEquals('fgae', $output[0]);
+    }
+    
+    public function testGenerateMap()
+    {
+        $solver             = new Day08Helper([]);
+        $row                = 'acedgfb cdfbe gcdfa fbcad dab cefabd cdfgeb eafb cagedb ab | cdfeb fcadb cdfeb cdbaf';
+        [$input, $output]   = $solver->publicParseRow($row);
+        $map                = $solver->publicGenerateMap($input);
+
+        $this->assertIsArray($map);
+        
+        $this->assertEquals('d', $map[0]);
+        $this->assertEquals('e', $map[1]);
+        $this->assertEquals('a', $map[2]);
+        $this->assertEquals('f', $map[3]);
+        $this->assertEquals('g', $map[4]);
+        $this->assertEquals('b', $map[5]);
+        $this->assertEquals('c', $map[6]);
+    }
+    
 }
